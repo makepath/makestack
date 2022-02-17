a7a9f7d2b0440e01fb3b487278cbffc8c6d41830
Filipe Limberger <lipilimb07@gmail.com>
2022-02-17 16:49:19 -0300

    add mock server for testing

---
+++ frontend/src/mocks/handlers.js
@@ -0,0 +1,14 @@
+import { rest } from 'wms';
+
+export const handlers = [
+    rest.get("http://localhost:8000/services", (req, res, ctx) => {
+        return res(
+            ctx.json([
+                { "client_url": "/world-countries-tile/tile/{z}/{x}/{y}", "default_extent": [-20037508.3427892, -20037508.3427892, 20037508.3427892, 20037508.3427892], "default_height": 256, "default_url": "/world-countries-tile/tile/0/0/0", "default_width": 256, "key": "world-countries-tile", "legend_name": "World Countries tile-legend", "legend_url": "/world-countries-tile/legend", "name": "World Countries tile", "service_page_name": "/world-countries-tile-tile", "service_page_url": "/world-countries-tile", "type": "tile", "service_url": "/world-countries-tile/tile///" },
+                { "client_url": "/world-countries-wms?bbox={XMIN},{YMIN},{XMAX},{YMAX}&width=256&height=256", "default_extent": [-20037508.3427892, -20037508.3427892, 20037508.3427892, 20037508.3427892], "default_height": 256, "default_url": "/world-countries-wms?bbox=-20037508.3427892,-20037508.3427892,20037508.3427892,20037508.3427892&width=256&height=256", "default_width": 256, "key": "world-countries-wms", "legend_name": "World Countries wms-legend", "legend_url": "/world-countries-wms/legend", "name": "World Countries wms", "service_page_name": "/world-countries-wms-wms", "service_page_url": "/world-countries-wms", "type": "wms", "service_url": "/world-countries-wms/wms" },
+                { "client_url": "/world-countries-image/image/{XMIN}/{YMIN}/{XMAX}/{YMAX}/{width}/{height}", "default_extent": [-20037508.3427892, -20037508.3427892, 20037508.3427892, 20037508.3427892], "default_height": 256, "default_url": "/world-countries-image/image/-20037508.3427892/-20037508.3427892/20037508.3427892/20037508.3427892/256/256", "default_width": 256, "key": "world-countries-image", "legend_name": "World Countries image-legend", "legend_url": "/world-countries-image/legend", "name": "World Countries image", "service_page_name": "/world-countries-image-image", "service_page_url": "/world-countries-image", "type": "image", "service_url": "/world-countries-image/image//////" },
+                { "client_url": "/world-countries-geojson/geojson", "default_extent": [-20037508.3427892, -20037508.3427892, 20037508.3427892, 20037508.3427892], "default_height": 256, "default_url": "/world-countries-geojson/geojson", "default_width": 256, "key": "world-countries-geojson", "legend_name": "World Countries geojson-legend", "legend_url": "/world-countries-geojson/legend", "name": "World Countries geojson", "service_page_name": "/world-countries-geojson-geojson", "service_page_url": "/world-countries-geojson", "type": "geojson", "service_url": "/world-countries-geojson/geojson" },
+            ])
+        )
+    })
+]
--- frontend/src/setupTests.ts
+++ frontend/src/setupTests.ts
@@ -3,6 +3,13 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom/extend-expect';
+import { server } from './mocks/server';
+
+beforeAll(() => server.listen())
+
+afterEach(() => server.resetHandlers())
+
+afterAll(() => server.close())
 
 if (typeof window.URL.createObjectURL === 'undefined') {
   const fn = (obj: Blob | MediaSource): string => {
