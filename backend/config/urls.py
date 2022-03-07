0ec49c410a055b6a02f3985f0f07eefdbf8a8c0c
giancastro <giancastrok@gmail.com>
2022-03-07 15:29:30 -0300

    add static files to urls

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -14,6 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path, re_path
 from django.views.generic import TemplateView
@@ -41,6 +42,9 @@ urlpatterns = [
 
 if env.bool("BACKEND_SILK", False):
     urlpatterns += [path("silk/", include("silk.urls", namespace="silk"))]
+
+if env.bool("BACKEND_DEBUG", False):
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 urlpatterns += [re_path(r"^", views.ReactAppView.as_view())]
 
