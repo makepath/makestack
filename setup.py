6df80f42eb267294b949608e0ae48fce2e0d3e0a
giancastro <giancastrok@gmail.com>
2023-01-10 13:59:53 -0300

    add qprompt to install requires

--- setup.py
+++ setup.py
@@ -4,7 +4,11 @@ setup(
     name="makestack",
     version="0.1",
     packages=find_packages(),
-    install_requires=["click >=8.1.3", "click-plugins >=1.1.1"],
+    install_requires=[
+        "click >=8.1.3",
+        "click-plugins >=1.1.1",
+        "qprompt==0.16.3",
+    ],
     entry_points="""
         [console_scripts]
         makestack=makestack:main
